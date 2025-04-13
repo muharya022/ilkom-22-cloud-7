4d2256830ce6a21c4bd31859826e077d231aa642
muharya022 <muhammadaryaalfajar@gmail.com>
2025-04-13 20:37:32 +0700

    setingg linter break newline

--- sistem_laporan/sistem_laporan/views.py
+++ sistem_laporan/sistem_laporan/views.py
@@ -1,25 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        
+        username = request.POST["username"]
+        password = request.POST["password"]
+
         user = authenticate(request, username=username, password=password)
-        
+
         if user is not None:
             login(request, user)
-            if user.is_staff:  
-                return redirect('admin_dashboard')
-            else:  
-                return redirect('dashboard')
+            if user.is_staff:
+                return redirect("admin_dashboard")
+            else:
+                return redirect("dashboard")
         else:
             messages.error(request, "Username atau password salah!")
 
-    return render(request, 'login.html')
+    return render(request, "login.html")
+
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
+
+
+@login_required
+def profil(request):
+    user = request.user
+    return render(request, "profil.html", {"user": user})
