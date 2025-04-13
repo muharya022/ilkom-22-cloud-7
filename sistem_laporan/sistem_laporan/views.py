6659bef6ef42398c542b1b73c637f589657ffa86
muharya022 <muhammadaryaalfajar@gmail.com>
2025-04-13 19:42:36 +0700

    fungsi login dan logout

---
+++ sistem_laporan/sistem_laporan/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            if user.is_staff:  
+                return redirect('admin_dashboard')
+            else:  
+                return redirect('dashboard')
+        else:
+            messages.error(request, "Username atau password salah!")
+
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
