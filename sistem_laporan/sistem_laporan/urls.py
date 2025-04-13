6214b58b64a5505ba2ab060e232d9b6243613245
muharya022 <muhammadaryaalfajar@gmail.com>
2025-04-13 19:30:04 +0700

    urls project

--- sistem_laporan/sistem_laporan/urls.py
+++ sistem_laporan/sistem_laporan/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for sistem_laporan project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import register_view, profil, login_view, logout_view, register_view, dashboard, admin_dashboard
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),   
+    path('', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('profil/', profil, name='profil'),
+    path('profil_admin/', views.profil_admin, name='profil_admin'),
+    path('dokumen/', include('dokumen.urls')),
 ]
