38c07550c12204a537814a5afb3287eade054041
muharya022 <muhammadaryaalfajar@gmail.com>
2025-04-13 20:10:41 +0700

    gitignore tes

--- sistem_laporan/sistem_laporan/urls.py
+++ sistem_laporan/sistem_laporan/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import register_view, profil, login_view, logout_view, register_view, dashboard, admin_dashboard
+from .views import profil, login_view, logout_view, register_view, dashboard, admin_dashboard
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -15,4 +15,4 @@ urlpatterns = [
     path('profil/', profil, name='profil'),
     path('profil_admin/', views.profil_admin, name='profil_admin'),
     path('dokumen/', include('dokumen.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
