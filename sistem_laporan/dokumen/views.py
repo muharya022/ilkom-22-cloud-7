c2bb9abb9ec3a1d2ae32ba46418aab8267a34dc9
wahyu285 <wahyuramadanraha@gmail.com>
2025-04-14 18:02:05 +0700

    fungsi hapus pengguna

--- sistem_laporan/dokumen/views.py
+++ sistem_laporan/dokumen/views.py
@@ -155,3 +155,13 @@ def register_view(request):
             messages.error(request, "Password tidak cocok.")
 
     return render(request, 'register.html')
+
+def is_admin(user):
+    return user.is_superuser or user.is_staff
+
+@user_passes_test(is_admin)
+def hapus_pengguna(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.delete()
+    messages.success(request, "Pengguna berhasil dihapus.")
+    return redirect('admin_dashboard')
