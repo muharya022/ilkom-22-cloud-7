4714e7cda7a958df5390fe895e4a6d6bad91f17e
deni2204 <apasjaboleh@gmail.com>
2025-04-14 20:04:21 +0800

    views register

--- sistem_laporan/sistem_laporan/views.py
+++ sistem_laporan/sistem_laporan/views.py
@@ -40,3 +40,32 @@ def is_admin(user):
 @user_passes_test(is_admin, login_url='dashboard')
 def profil_admin(request):
     return render(request, 'profil_admin.html', {'user': request.user})
+
+
+def register_view(request):
+    if request.method == "POST":
+        full_name = request.POST['full_name']
+        first_name, last_name = full_name.split(' ', 1) if ' ' in full_name else (full_name, '')
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username sudah digunakan.")
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, "Email sudah digunakan.")
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.first_name = first_name
+                user.last_name = last_name
+                user.save()  
+
+                messages.success(request, "Pendaftaran berhasil! Silakan login.")
+                return redirect('login')
+        else:
+            messages.error(request, "Password tidak cocok.")
+
+    return render(request, 'register.html')
