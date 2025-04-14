9c724c1ecad526d756c1493200085e76df7051ae
indah1107 <puspitasari110704@gmail.com>
2025-04-14 18:29:37 +0800

    forms dokument

--- sistem_laporan/dokumen/forms.py
+++ sistem_laporan/dokumen/forms.py
@@ -11,4 +11,11 @@ class DokumenForm(forms.ModelForm):
         ("IRBAN INVESTIGASI", "IRBAN INVESTIGASI"),
     ]
 
-    
+    class Meta:
+        model = Dokumen
+        fields = ["nomor_surat", "tanggal_surat", "irban", "tim_audit", "uraian", "file"]
+        widgets = {
+            "nomor_surat": forms.TextInput(attrs={"class": "form-control", "placeholder": "Masukkan nomor surat"}),
+            "tanggal_surat": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+            "file": forms.ClearableFileInput(attrs={"class": "form-control"}),
+        }
